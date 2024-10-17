333533e63d106460c45248b369154cb57beba167
FABIAN RAMIREZ <fabian.ramirez@nowports.com>
2024-10-16 22:33:29 -0500

    feat: implement test table suite simpleCalculator

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,37 +1,37 @@
 // Uncomment the code below and write your tests
-import { simpleCalculator } from './index'; //Action
+import { simpleCalculator, Action } from './index'; //Action
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    const input = { a: 10, b: 5, action: '+' };
+    const input = { a: 10, b: 5, action: Action.Add };
     const result = simpleCalculator(input);
 
     expect(result).toEqual(15);
   });
 
   test('should subtract two numbers', () => {
-    const input = { a: 10, b: 5, action: '-' };
+    const input = { a: 10, b: 5, action: Action.Subtract };
     const result = simpleCalculator(input);
 
     expect(result).toEqual(5);
   });
 
   test('should multiply two numbers', () => {
-    const input = { a: 10, b: 5, action: '*' };
+    const input = { a: 10, b: 5, action: Action.Multiply };
     const result = simpleCalculator(input);
 
     expect(result).toEqual(50);
   });
 
   test('should divide two numbers', () => {
-    const input = { a: 10, b: 5, action: '/' };
+    const input = { a: 10, b: 5, action: Action.Divide };
     const result = simpleCalculator(input);
 
     expect(result).toEqual(2);
   });
 
   test('should exponentiate two numbers', () => {
-    const input = { a: 10, b: 5, action: '^' };
+    const input = { a: 10, b: 5, action: Action.Exponentiate };
     const result = simpleCalculator(input);
 
     expect(result).toEqual(100000);
@@ -45,7 +45,7 @@ describe('simpleCalculator tests', () => {
   });
 
   test('should return null for invalid arguments', () => {
-    const input = { a: 'x', b: 'y', action: '+' };
+    const input = { a: 'x', b: 'y', action: Action.Add };
     const result = simpleCalculator(input);
 
     expect(result).toEqual(null);
